46c6616217ae6b97d00dbb57e6785fb18976ce3c
Jack <Jwenschhof@bsd220.org>
2022-04-20 10:53:26 -0500

    New View

---
+++ Guess That Country/Countries .swift
@@ -0,0 +1,21 @@
+//
+//  Countries .swift
+//  Guess That Country
+//
+//  Created by Student on 4/20/22.
+//
+
+import SwiftUI
+
+struct Countries_: View {
+    var Countries = ["Country_0", "Country_1", "Country_2", "Country_3", "Country_4", "Country_5", "Country_6", "Country_7", "Country_8", "Country_9", "Country_10", "Country_11", "Country_12", "Country_13", "Country_14", "Country_15", "Country_16", "Country_17", "Country_18", "Country_19", "Country_20", "Country_21", "Country_22", "Country_23", "Country_24", "Country_25", "Country_26", "Country_27", "Country_28", "Country_29", "Country_30", "Country_31", "Country_32", "Country_33", "Country_34", "Country_35", "Country_36", "Country_37", "Country_38", "Country_39", "Country_40", "Country_41"].shuffled()
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Countries__Previews: PreviewProvider {
+    static var previews: some View {
+        Countries_()
+    }
+}
