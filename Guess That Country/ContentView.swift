b8ff40585a9e0cf695b13f3ce769c39bd0bbb887
Jack <Jwenschhof@bsd220.org>
2022-04-20 10:16:44 -0500

    Made an array of countries

--- Guess That Country/ContentView.swift
+++ Guess That Country/ContentView.swift
@@ -40,6 +40,7 @@ struct ThirdContentView: View {
     var body: some View {
         Text("Guess the Country")
     }
+    var countries = ["Country_0", "Country_1", "Country_2", "Country_3", "Country_4", "Country_5", "Country_6", "Country_7", "Country_8", "Country_9", "Country_10", "Country_11", "Country_12", "Country_13", "Country_14", "Country_15", "Country_16", "Country_17", "Country_18", "Country_19", "Country_20", "Country_21", "Country_22", "Country_23", "Country_24", "Country_25", "Country_26", "Country_27", "Country_28", "Country_29", "Country_30", "Country_31", "Country_32", "Country_33", "Country_34", "Country_35", "Country_36", "Country_37", "Country_38", "Country_39", "Country_40", "Country_41"].shuffled()
 }
 
 struct ContentView_Previews: PreviewProvider {
